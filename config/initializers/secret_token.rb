bfdd8aa7129d63c114a1d0128cc38cf4eebbf0f3
Andrej Antas <andrej.antas@gmail.com>
2013-10-18 11:19:10 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinitestTurnDemo::Application.config.secret_key_base = '1f1b6b071b1bde0a26d18c3cb434b5f233b017a285be91096a746a1cd3f43d641996fc9ef33122bef3e4bbcf8710035f9257fbfa0fe6222f6d3f73602ce9d6b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MinitestTurnDemo::Application.config.session_store :cookie_store, key: '_minitest_turn_demo_session'
